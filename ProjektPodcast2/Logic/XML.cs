1410eac48136c1778e39230173124df53359e461
Esho Odisho <esho96@hotmail.com>
2017-10-28 14:12:01 +0200

    xml

---
+++ ProjektPodcast2/Logic/XML.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Logic
+{
+    class XML
+    {
+        public void Main(string[] arg)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Data.Category));
+            TextReader reader = new StreamReader(@"C:\myXml.xml");
+            object obj = deserializer.Deserialize(reader);
+            Data.Category XmlData = (Data.Category)obj;
+            reader.Close();
+        }
+    }
+}
